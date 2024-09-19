8e6466c68e0d832c064e1c192de7978939ac134b
Daniel Chin <DanChin327@gmail.com>
2024-09-19 16:07:17 +0900

    pract

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS #preventing the Cors errors: prevents errors during transfer from website to website.
 import sqlalchemy
 
@@ -21,9 +21,33 @@ def get_comments():
         print("result.all()", result.all(), "-----", type(result.all()))
     return result.all()
 
+# comment_id, username, comment_text
+def add_comment(username, comment_text):
+    with db.connect() as conn:
+        result = conn.execute(sqlalchemy.text("""
+        INSERT INTO `comments` (`comment_id`, `username`, `comments_txt`) VALUES (NULL, :username, :comment_text) """),
+        {
+            "username": username,
+            "comment_text": comment_text,  # This is the corrected key
+        })
+        conn.commit()
+    return get_comments()
 
+@app.route('/api/comment', methods = ["POST"])
+def add_comment_route():
+    data = request.get_json()
+    print(data)
+    username = data.get("username")
+    comment = data.get('comments_txt')
+
+    add_comment(username, comment)
+
+    return jsonify({'message': 'comment succesfully added'}), 200
+
+# @app.route('/api/comment', methods = ["GET"])
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    get_comments()
+    app.run(debug=True)
+    # get_comments()
+    # add_comment("Stacy", "This is Stacy's comment")
